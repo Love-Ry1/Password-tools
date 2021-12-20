75ccd54a7d63bab1bd950de1d2e895c90737bace
Love-Ry1 <83970045+Love-Ry1@users.noreply.github.com>
2021-12-20 20:25:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from time import perf_counter
 from functools import wraps
 import string
 from itertools import product
+from os import listdir
 
 
 def time_func(function):    # wrapper function to time other functions
@@ -18,6 +19,23 @@ def user_password():    # returns the password the user want to use
     pw = input("Enter a password: ")
     return pw
     # TODO only accept some characters (a-z), (1-9) etc, max characters
+
+
+def choose_pwlist():
+    pwlists = listdir("password_lists")
+    for x in range(len(pwlists)):   # Is there a moore pythonic way to do this?
+        print(f"{x+1}: {pwlists[x]}")
+
+    while True:  # valid-input-loop
+        try:
+            number = int(input("Please enter the number of the password-list you want to use: "))
+            if number <= len(pwlists):
+                break
+        except Exception:
+            print("Invalid input")
+
+    pwlist_name = pwlists[number - 1]
+    return pwlist_name
 
 
 @time_func
@@ -63,23 +81,25 @@ def compare_pwlist(pw, pw_list_path):     # Compares the password with password 
 if __name__ == '__main__':
     password = None
     nmb_of_char = 10
+    pwlist_path = "password_lists/100k_common.txt"
     while True:     # main-loop
-        while True: # pick-an-option-loop
+        while True:     # pick-an-option-loop
             try:
                 choice = int(input("Please pick an option:\n1. Try your password against a password-list\n"
                                    "2. Try to bruteforce your password\n"
                                    "3. Set password\n"
-                                   "4. Set character bruteforce limit\nAnswer: "))
+                                   "4. Set character bruteforce limit\n"
+                                   "5. Set a password-list\nAnswer: "))
                 break
-            except:
+            except Exception:
                 print("Invalid option")
 
-        if password is None and choice != 3:    # works but bit weird way to do it
+        if password is None and not (choice == 3 or choice == 5):    # doesn't feel good
             password = user_password()
             print("not none")
 
         if choice == 1:
-            compare_pwlist(password, "password_lists/100k_common.txt")
+            compare_pwlist(password, pwlist_path)
         elif choice == 2:
             bruteforce_password(nmb_of_char, password)
         elif choice == 3:
@@ -89,18 +109,20 @@ if __name__ == '__main__':
             while True:     # valid-input-loop
                 try:
                     print(f"Current limit is: {nmb_of_char}")
-                    nmb_of_char = int(input("Up to how many characters do you want to bruteforce? "))   # Maybe unnecessary?
+                    nmb_of_char = int(input("Up to how many characters do you want to bruteforce? "))
                     break
-                except:
+                except Exception:
                     print("Invalid input")
+        elif choice == 5:
+            current_pwlist = pwlist_path.split('/')     # works for this project but not water-proof...
+            print(f"Current password-list is: {current_pwlist[1]}")
+            pwlist_path = "password_lists/" + choose_pwlist()
 
 """
     TODO
     add keylistener to cancel for example the bruteforce
-    add ability to use different wordlists
     add recommended password
     add range to bruteforce (like 3-6 characters)
-    add save password and range (like set one time)
     add save output to txt-file    
     add error handling
     maybe add function that take question string as parameter to move the loops from main into functions? makes main look more clean
